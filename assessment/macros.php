23a54b9e2ddb6a9dcd4b1cd84a39cc7f3ed3d704
Themid1 <127655716+Themid1@users.noreply.github.com>
2025-07-28 18:48:53 +0700

    change

--- assessment/macros.php
+++ assessment/macros.php
@@ -723,14 +723,15 @@ function showplot($funcs) { //optional arguments:  $xmin,$xmax,$ymin,$ymax,label
 		$function_names[] = trim($f);
 	}
 	$function_list_str = '["' . implode('","', array_map('addslashes', $function_names)) . '"]';
+	// $function_list_str = makepretty($function_list_str);
 	$function_list_str = makexpretty($function_list_str);
-	$function_list_str = makexxpretty($function_list_str);
+	// $function_list_str = makexxpretty($function_list_str);
 	// $commands = "var function_list = $function_list_str; " . $commands;
 
 	if ($_SESSION['graphdisp']==0) {
 		return $alt;
 	} else {
-		return "<embed type='image/svg+xml' align='middle' width='$plotwidth' 'height='$plotheight' var function_list='$function_list_str' script='$commands' />\n";
+		return "<embed type='image/svg+xml' align='middle' width='$plotwidth' 'random' = '$new_test_only' height='$plotheight' var function_list='$function_list_str' script='$commands' />\n";
 	}
 }
 

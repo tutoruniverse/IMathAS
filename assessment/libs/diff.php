f73a0df30ff58dc7a99ffecc31bf3d84d20594e1
David Lippman <drlippman@yahoo.com>
2026-07-09 11:24:24 -0700

    fix input order for diff

--- assessment/libs/diff.php
+++ assessment/libs/diff.php
@@ -9,7 +9,7 @@ array_push(
     'diffbycharhtml'
 );
 
-function diffbychar($new,$old) {
+function diffbychar($old,$new) {
     $d = diff(str_split($old), str_split($new));
     // seems to put empty edits at the start and/or end sometimes; strip those
     foreach ($d as $k=>$v) {
@@ -20,14 +20,15 @@ function diffbychar($new,$old) {
     return array_values($d);
 }
 
-function diffbycharhtml($new,$old,$format=0) {
+function diffbycharhtml($old,$new,$format=3) {
+    // format: 1: wrap ins, 2: wrap dec, 3: wrap both 
     $diff = diff(str_split($old), str_split($new));
     $ret = '';
 	foreach($diff as $k){
 		if(is_array($k))
-			$ret .= (!empty($k['d'])?"<del>".implode(' ',$k['d'])."</del> ":'').
-				(!empty($k['i'])?"<ins>".implode(' ',$k['i'])."</ins> ":'');
-		else $ret .= $k . ' ';
+			$ret .= ((!empty($k['d']) && $format&2)?"<del>".implode('',$k['d'])."</del>":'').
+				((!empty($k['i']) && $format&1)?"<ins>".implode('',$k['i'])."</ins>":'');
+		else $ret .= $k;
 	}
 	return $ret;
 }

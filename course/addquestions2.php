792a7a1eb7ee38084a2ea6180f33ebf995c61925
David Lippman <drlippman@yahoo.com>
2026-09-02 11:25:41 -0700

    bug fix: inline tiny toolbar hidden by "with selected" stickyonscroll on addquestions2

--- course/addquestions2.php
+++ course/addquestions2.php
@@ -392,7 +392,7 @@ if (!(isset($teacherid))) { // loaded by a NON-teacher
     $placeinhead .= "<script type=\"text/javascript\" src=\"$staticroot/javascript/junkflag.js?v=021326\"></script>";
     $placeinhead .= "<script type=\"text/javascript\" src=\"$staticroot/javascript/DatePicker.js?v=080818\"></script>";
 	$placeinhead .= "<script type=\"text/javascript\">var JunkFlagsaveurl = '". $GLOBALS['basesiteurl'] . "/course/savelibassignflag.php';</script>";
-    $placeinhead .= "<link rel=\"stylesheet\" href=\"$staticroot/course/addquestions2.css?v=081326\" type=\"text/css\" />";
+    $placeinhead .= "<link rel=\"stylesheet\" href=\"$staticroot/course/addquestions2.css?v=090226\" type=\"text/css\" />";
     $placeinhead .= '<script>
         $(function() {
             if (window.top != window.self) {
